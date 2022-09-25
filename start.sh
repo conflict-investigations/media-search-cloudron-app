244f24b231acb86296b4c310f20f8cd54e89bff7
Anonymous <none@none.none>
2022-09-25 17:08:48 +0200

    CloudronManifest, start.sh: Add logging for gunicorn

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ cd /app/data
 
 mkdir -p data/ dump/
 
+# Create temporary log dir
+mkdir -p /run/app
+
 echo "=> Ensuring permissions"
 chown -Rh cloudron:cloudron /app/data
 chown -Rh cloudron:cloudron /app/code
@@ -13,4 +16,10 @@ echo "=> Fetching files from the internet"
 /usr/local/bin/gosu cloudron:cloudron media_search -o
 
 echo "=> Starting web app"
-exec /usr/local/bin/gosu cloudron:cloudron ${VIRTUAL_ENV}/bin/gunicorn -b 0.0.0.0:8000 -w 2 'media_search.web:app'
+exec /usr/local/bin/gosu cloudron:cloudron \
+    ${VIRTUAL_ENV}/bin/gunicorn \
+        -b 0.0.0.0:8000 \
+        -w 2 \
+        --access-logfile /run/app/gunicorn-access.log \
+        --error-logfile /run/app/gunicorn-error.log \
+        'media_search.web:app'
